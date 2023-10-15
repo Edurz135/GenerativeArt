8e7c63ab20d2c8b8a1cc9bdaaeb24b826924ca3c
RBrunoChV <edu.rz@hotmail.com>
2023-10-14 19:04:50 -0500

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { RandomWalker } from "./views/pages/randomWalker/randomWalker.page";
+import { MainPage } from "./views/pages/mainPage/mainPage.page";
 
 function App() {
   return (
     <div>
-      <RandomWalker />
+      <MainPage />
     </div>
   );
 }
---
+++ src/views/pages/mainPage/mainPage.page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import reference from "../../../";
+
+export const MainPage = () => {
+  return <div>a</div>;
+};
